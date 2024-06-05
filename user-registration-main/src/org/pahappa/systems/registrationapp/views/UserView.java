6408c131602d97812d6db710fbe154d2c8f1f591
masembe0757 <sendyj886@gmail.com>
2024-06-05 15:18:43 +0300

    Functionality implemented

--- user-registration-main/src/org/pahappa/systems/registrationapp/views/UserView.java
+++ user-registration-main/src/org/pahappa/systems/registrationapp/views/UserView.java
@@ -1,7 +1,13 @@
 package org.pahappa.systems.registrationapp.views;
+import org.pahappa.systems.registrationapp.models.User;
 import  org.pahappa.systems.registrationapp.services.UserService;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class UserView {
@@ -30,38 +36,23 @@ public class UserView {
                 scanner.nextLine(); // Consume the newline character
                 switch (choice) {
                     case 1:
-                        Method m = UserService.class.getDeclaredMethod("registerUser");
-                        m.setAccessible(true);
-                        m.invoke(user_service);
+                        registerUser();
                         break;
                     case 2:
-                        Method m1 = UserService.class.getDeclaredMethod("displayAllUsers");
-                        m1.setAccessible(true);
-                        m1.invoke(user_service);
+                        displayAllUsers();
                         break;
                     case 3:
-                        //getUserOfUsername();
-                        Method m2 = UserService.class.getDeclaredMethod("getUserOfUsername");
-                        m2.setAccessible(true);
-                        m2.invoke(user_service);
+                        getUserOfUsername();
                         break;
                     case 4:
-                        //updateUserOfUsername();
-                        Method m3 = UserService.class.getDeclaredMethod("updateUserOfUsername");
-                        m3.setAccessible(true);
-                        m3.invoke(user_service);
+                        updateUserOfUsername();
                         break;
                     case 5:
-                       // deleteUserOfUsername();
-                        Method m4 = UserService.class.getDeclaredMethod("deleteUserOfUsername");
-                        m4.setAccessible(true);
-                        m4.invoke(user_service);
+                        deleteUserOfUsername();
+
                         break;
                     case 6:
-                        //deleteAllUsers();
-                        Method m5 = UserService.class.getDeclaredMethod("displayAllUsers");
-                        m5.setAccessible(true);
-                        m5.invoke(user_service);
+                        deleteAllUsers();
                         break;
                     case 7:
                         running = false;
@@ -74,6 +65,53 @@ public class UserView {
                 scanner.nextLine(); // Consume the newline character
             }
         }
+    }
+
+
+    private void registerUser() throws ParseException {
+
+        System.out.println("Enter users first name");
+        String first_name = scanner.nextLine();
+
+        System.out.println("Enter users last name");
+        String last_name = scanner.nextLine();
+
+        System.out.println("Enter users username");
+        String user_name = scanner.nextLine();
+
+        System.out.println("Enter users date of birth format mm/dd/yyyy ");
+        String date_of_birth = scanner.nextLine();
+        DateFormat df = new SimpleDateFormat("mm/dd/yyyy");
+        Date date_of_birt = df.parse(date_of_birth);
+
+        user_service.addUser(first_name, last_name, user_name, date_of_birt);
+    }
+
+    private void displayAllUsers() {
+        user_service.returnAllUsers();
+    }
+
+    private void getUserOfUsername() {
+        System.out.println("Enter user's username");
+        String user_name = scanner.nextLine();
+        user_service.returnUserOfUserName(user_name);
+    }
+
+    private void updateUserOfUsername() throws ParseException {
+        System.out.println("Enter user's username");
+        String user_name = scanner.nextLine();
+        user_service.updateUserOfUserName(user_name);
+
+    }
+
+    private void deleteUserOfUsername() {
+        System.out.println("Enter user's username you with to delete");
+        String user_name = scanner.nextLine();
+        user_service.deleteUserOfUserName(user_name);
+    }
+
+    private void deleteAllUsers() {
+        user_service.deleteAllUsers();
     }
 
 
